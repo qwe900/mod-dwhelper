b561ee12fe04f9cfd436a7045c30d6de0ff4085f
qwe900 <qwe900@web.de>
2024-05-28 08:49:23 +0200

    Update dwhelper.cpp

--- src/dwhelper.cpp
+++ src/dwhelper.cpp
@@ -4,8 +4,6 @@
 #include "Creature.h"
 #include "ScriptedGossip.h"
 #include "ScriptedCreature.h"
-#include "SpellScript.h"
-#include "SpellScriptLoader.h"
 
 enum DreamwalkerHealer
 {
@@ -21,43 +19,37 @@ enum Actions
     ACTION_ENTER_COMBAT = 1,
     MISSED_PORTALS = 2,
     ACTION_DEATH = 3,
-}; 
+};
+
 enum Spells
 {
-    //Class Paladin 
-    SPELL_BEACON_OF_LIGHT = 53563, 
-    SPELL_HOLY_LIGHT = 48782, 
+    // Class Paladin 
+    SPELL_BEACON_OF_LIGHT = 53563,
+    SPELL_HOLY_LIGHT = 48782,
     SPELL_FLASH_OF_LIGHT = 48785,
-    SPELL_HOLY_SHOCK = 48824, 
-    SPELL_SACRED_SHIELD = 53601, 
-    SPELL_CLEANSE = 4987, 
-    SPELL_LAY_ON_HANDS = 48788, 
-    SPELL_DIVINE_FAVOR = 20216, 
-    SPELL_DIVINE_ILLUMINATION = 31842, 
+    SPELL_HOLY_SHOCK = 48824,
+    SPELL_SACRED_SHIELD = 53601,
+    SPELL_CLEANSE = 4987,
+    SPELL_LAY_ON_HANDS = 48788,
+    SPELL_DIVINE_FAVOR = 20216,
+    SPELL_DIVINE_ILLUMINATION = 31842,
     SPELL_HAND_OF_SACRIFICE = 6940,
     AURA_DREAMSTATE = 70766,
-
 };
 
 class npc_dreamwalker_helper : public CreatureScript
 {
 public:
-    npc_dreamwalker_helper() : CreatureScript("npc_dreamwalker_helper_chooser") { }
+    npc_dreamwalker_helper() : CreatureScript("npc_dreamwalker_helper") { }
 
     bool OnGossipHello(Player* player, Creature* creature) override
     {
-        // Check if the current instance is Icecrown Citadel and if Sindragosa is defeated
         if (InstanceScript* instance = creature->GetInstanceScript())
         {
-
             if (instance->GetBossState(DATA_VALITHRIA_DREAMWALKER) != DONE)
             {
-
-
-
                 AddGossipItemFor(player, GOSSIP_ICON_INTERACT_1, "|cff00ff00|TInterface\\icons\\spell_holy_holybolt:30|t|r Ich wünsche mir Hilfe von Anni", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 1);
                 AddGossipItemFor(player, GOSSIP_ICON_INTERACT_1, "|cff00ff00|TInterface\\icons\\spell_nature_magicimmunity:30|t|r Ich wünsche mir Hilfe von Corilla", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 2);
-                //second healer is not implemented yet
                 SendGossipMenuFor(player, 50001, creature->GetGUID());
                 return true;
             }
@@ -80,33 +72,22 @@ public:
         {
         case GOSSIP_ACTION_INFO_DEF + 1:
             helperEntry = 650001;
-
             break;
         case GOSSIP_ACTION_INFO_DEF + 2: //second healer is not implemented yet
             helperEntry = 650001;
-
             break;
         default:
             return false;
-            break;
         }
+
         if (!player->FindNearestCreature(650001, 500.0f) && !player->FindNearestCreature(650002, 500.0f))
         {
-
             Position pos;
             creature->GetNearPoint(creature, pos.m_positionX, pos.m_positionY, pos.m_positionZ, 0, 5.0f, 0);
             if (Creature* helper = player->SummonCreature(helperEntry, pos, TEMPSUMMON_TIMED_OR_DEAD_DESPAWN, 1000 * 60 * 60))
             {
-
                 helper->GetMotionMaster()->MoveFollow(player, PET_FOLLOW_DIST, PET_FOLLOW_ANGLE);
             }
-
-
-
-
-
-
-
 
             CloseGossipMenuFor(player);
             return true;
@@ -127,33 +108,34 @@ public:
 
     struct npc_DW_HelperAI : public ScriptedAI
     {
+        npc_DW_HelperAI(Creature* creature) : ScriptedAI(creature), _instance(creature->GetInstanceScript())
+        {
+            me->SetStat(STAT_INTELLECT, 20000);
+        }
+
         void Reset() override
         {
             _events.Reset();
             _events.ScheduleEvent(EVENT_HEALING, 1000); // Schedule healing event
-
-
         }
+
         std::vector<std::string> randomTexts = {
-       "Hodor ! Hold The Door!",
-       "I am going to stack up , please Protect Dreamwalker ",
-       "PAW PAW PAW PAW PATROL !",
-       "Life is like a box of chocolates. You never know what you're gonna get.",
-       "May the Force be with you."
+            "Hodor ! Hold The Door!",
+            "I am going to stack up , please Protect Dreamwalker",
+            "PAW PAW PAW PAW PATROL !",
+            "Life is like a box of chocolates. You never know what you're gonna get.",
+            "May the Force be with you."
         };
 
-        npc_DW_HelperAI(Creature* creature) : ScriptedAI(creature), _instance(creature->GetInstanceScript()) {
-
-            me->SetStat(STAT_INTELLECT, 20000);
-
-
-        }
         bool inVoid = false;
         bool _portalfound = false;
         bool OnMovement = false;
         bool meIsInCombat = false;
+        Position StandardPosition = Position(4233.2495, 2484.4174, 364.872192, M_PI);
+
         EventMap _events;
         InstanceScript* _instance;
+
         enum Events
         {
             EVENT_PORTAL = 1,
@@ -161,18 +143,20 @@ public:
             EVENT_CLICK_PORTAL = 3,
             EVENT_HEALING = 4
         };
+
         enum Points
         {
             POINT_PORTAL = 1,
             POINT_TARGET = 2
         };
-        enum Creatures {
+
+        enum Creatures
+        {
             VoidSpawnedBeforePortal = 38186,
             PortalToUse = 37945,
             CloudToCollectNonHeroic = 37985,
             CloudToCollectHeroic = 38421
         };
-
 
         Unit* SelectLowestFriendlyHpTarget(float range)
         {
@@ -220,48 +204,45 @@ public:
 
         void JustEngagedWith(Unit* target) override
         {
-      
-
             me->setActive(true);
             me->SetInCombatWithZone();
             meIsInCombat = true;
             _instance->SendEncounterUnit(ENCOUNTER_FRAME_ENGAGE, me);
-          
+        }
 
-            
+        void GoToStandardPosition()
+        {
+            me->GetMotionMaster()->MovePoint(POINT_TARGET, StandardPosition);
         }
 
         void MovementInform(uint32 type, uint32 id) override
         {
             if (type != POINT_MOTION_TYPE)
                 return;
+
             if (id == POINT_TARGET)
             {
-                //_events.ScheduleEvent(EVENT_SEARCH_NEXT_TARGET, 500ms); // Schedule the event with a 500ms delay
-            }
-            if (type == IDLE_MOTION_TYPE) {
-                OnMovement = false;
+                // Schedule the event with a 500ms delay if needed
             }
 
+            if (type == IDLE_MOTION_TYPE)
+            {
+                OnMovement = false;
+            }
         }
-    
 
         void CastEnhancedHealSpell(Unit* target, uint32 spellId)
         {
             if (!target)
                 return;
 
-            // Cast the spell normally to respect the cast time
             me->CastSpell(target, spellId, false);
         }
 
         void UpdateAI(uint32 diff) override
         {
-            // Return if the NPC is moving
             if (me->HasUnitState(UNIT_STATE_MOVING))
                 return;
-
-
 
             _events.Update(diff);
             me->SetPower(POWER_MANA, 50000);
@@ -271,7 +252,6 @@ public:
                 switch (eventId)
                 {
                 case EVENT_CLICK_PORTAL:
-                {
                     if (Creature* portal = me->FindNearestCreature(PortalToUse, 100.0f))
                     {
                         portal->HandleSpellClick(me);
@@ -279,33 +259,28 @@ public:
                         _portalfound = false;
                     }
                     break;
-                }
                 case EVENT_SEARCH_NEXT_TARGET:
                     if (me->HasAura(AURA_DREAMSTATE))
                     {
                         me->SetDisableGravity(true);
-                        // Find the nearest creature with entry 37985 or 38421
                         Unit* target = me->FindNearestCreature(CloudToCollectNonHeroic, 100.0f);
                         if (!target)
                             target = me->FindNearestCreature(CloudToCollectHeroic, 100.0f);
 
-                        // If a target was found, fly to it if its not 2m away
                         if (target && me->GetDistance(target) > 2.0f)
                         {
-
                             me->SetSpeed(MOVE_RUN, me->GetCreatureTemplate()->speed_run * 1.5);
                             me->GetMotionMaster()->MovePoint(POINT_TARGET, target->GetPositionX(), target->GetPositionY(), target->GetPositionZ());
                         }
                     }
                     break;
-
                 case EVENT_HEALING:
-                {
-                    if (me->HasUnitState(UNIT_STATE_CASTING)) {
+                    if (me->HasUnitState(UNIT_STATE_CASTING))
+                    {
                         _events.ScheduleEvent(EVENT_HEALING, 1s);
                         break;
                     }
-                    // Keep Beacon of Light active on Valithria Dreamwalker
+
                     if (Creature* dreamwalker = me->FindNearestCreature(NPC_VALITHRIA_DREAMWALKER, 150.0f))
                     {
                         if (!dreamwalker->HasAura(SPELL_BEACON_OF_LIGHT))
@@ -316,45 +291,35 @@ public:
                         {
                             me->CastSpell(dreamwalker, SPELL_SACRED_SHIELD, false);
                         }
-
                     }
 
                     Unit* target = SelectLowestFriendlyHpTarget(40.0f);
 
-
-                    // Cast Holy Shock on cooldown
-                    if (!me->HasSpellCooldown(SPELL_HOLY_SHOCK)) {
-                        CastEnhancedHealSpell(target,SPELL_HOLY_SHOCK);
-                       me->AddSpellCooldown(SPELL_HOLY_SHOCK, 0, 8000);
+                    if (!me->HasSpellCooldown(SPELL_HOLY_SHOCK))
+                    {
+                        me->Yell("Holy Shock", LANG_UNIVERSAL);
+                        CastEnhancedHealSpell(target, SPELL_HOLY_SHOCK);
+                        me->AddSpellCooldown(SPELL_HOLY_SHOCK, 0, 8000);
                     }
-                    if (!OnMovement) {
-                        if (!me->HasSpellCooldown(SPELL_FLASH_OF_LIGHT)) {
-                             me->Yell("Flash of Light", LANG_UNIVERSAL);
+
+                    if (!OnMovement)
+                    {
+                        if (!me->HasSpellCooldown(SPELL_FLASH_OF_LIGHT))
+                        {
+                            me->Yell("Flash of Light", LANG_UNIVERSAL);
                             CastEnhancedHealSpell(target, SPELL_FLASH_OF_LIGHT);
                             me->AddSpellCooldown(SPELL_FLASH_OF_LIGHT, 0, 7000);
                         }
 
-                        //cast holy light on dw if other spells has cooldowns
-
                         CastEnhancedHealSpell(target, SPELL_HOLY_LIGHT);
-                        // me->Yell("Holy Light", LANG_UNIVERSAL);
-
-
                     }
-
 
                     _events.ScheduleEvent(EVENT_HEALING, 1s);
                     break;
-                }
                 default:
-
                     break;
                 }
             }
-
-
-
-
 
             if (Creature* voidZone = me->FindNearestCreature(VoidSpawnedBeforePortal, 100.0f))
             {
@@ -367,87 +332,34 @@ public:
                     _portalfound = true;
                     std::string randomYell = randomTexts[urand(0, randomTexts.size() - 1)];
                     me->Yell(randomYell.c_str(), LANG_UNIVERSAL);
-                    // Calculate a position that is 2 meters away from the portal
+
                     Position newPosition = voidZone->GetPosition();
                     newPosition.m_positionX += cos(voidZone->GetOrientation()) * 2;
                     newPosition.m_positionY += sin(voidZone->GetOrientation()) * 2;
                     OnMovement = true;
                     me->GetMotionMaster()->MovePoint(POINT_PORTAL, newPosition);
-                    //check if npc reached the portal
-
-
 
                     _events.ScheduleEvent(EVENT_CLICK_PORTAL, 16s);
                 }
             }
 
-
-            if (me->HasAura(70766)) {
+            if (me->HasAura(70766))
+            {
                 _events.ScheduleEvent(EVENT_SEARCH_NEXT_TARGET, 2500ms);
             }
-            else {
+            else
+            {
                 inVoid = false;
             }
-            if (!me->HasAura(70766) && me->GetMotionMaster()->GetCurrentMovementGeneratorType() != FOLLOW_MOTION_TYPE && !_portalfound) // Add meIsActive to the condition
+
+            if (!me->HasAura(70766) && me->GetMotionMaster()->GetCurrentMovementGeneratorType() != FOLLOW_MOTION_TYPE && !_portalfound)
             {
-                // Find the next friendly player within a 100-meter range
-                Creature* dw = me->FindNearestCreature(NPC_VALITHRIA_DREAMWALKER, 15.0f);
-
-                // Check if the found player is friendly and alive
-                if (dw && !dw->isDead() && me->IsFriendlyTo(dw))
-                {
-                    // Make the NPC follow the player
-                    me->GetMotionMaster()->MoveFollow(dw, 15.0f, PET_FOLLOW_ANGLE);
-                    me->SetOwnerGUID(dw->GetGUID());
-                    me->SetControlled(true, UNIT_STATE_FOLLOW);
-                    me->SetReactState(REACT_DEFENSIVE);
-                }
+                GoToStandardPosition();
+                me->SetReactState(REACT_DEFENSIVE);
             }
-        }
-
-
-
-
-    };
-    class spell_increase_heal : public SpellScriptLoader
-    {
-    public:
-        spell_increase_heal() : SpellScriptLoader("spell_increase_heal") { }
-
-        class spell_increase_heal_SpellScript : public SpellScript
-        {
-            PrepareSpellScript(spell_increase_heal_SpellScript);
-
-            void HandleAfterHit()
-            {
-                Unit* target = GetHitUnit();
-                if (!target)
-                    return;
-
-                int32 heal = GetHitHeal();
-                heal *= 4; // Increase healing by 400%
-                SetHitHeal(heal);
-            }
-
-            void Register() override
-            {
-                AfterHit += SpellHitFn(spell_increase_heal_SpellScript::HandleAfterHit);
-            }
-        };
-
-        SpellScript* GetSpellScript() const override
-        {
-            return new spell_increase_heal_SpellScript();
         }
     };
 
-    // In your script registration function
-
-
-    void AddSC_spell_increase_heal()
-    {
-        new spell_increase_heal();
-    }
     CreatureAI* GetAI(Creature* creature) const override
     {
         return new npc_DW_HelperAI(creature);
@@ -458,6 +370,4 @@ void AdddwhelperScripts()
 {
     new npc_dreamwalker_helper();
     new npc_DW_Helper();
-   
-   
 }
